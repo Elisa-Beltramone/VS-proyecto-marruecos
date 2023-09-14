06af498d7547f5825ad0b0191c2159428cd3cf32
Elisa <elibeltramone@gmail.com>
2023-09-14 18:24:31 +0200

    conversor

---
+++ marruecos.js
@@ -0,0 +1,14 @@
+  // Función para realizar la conversión
+  function convertirADirhams() {
+    const tasaDeCambio = 10.0; // Reemplaza con la tasa de cambio actual
+    const monto = parseFloat(document.getElementById('monto').value);
+    const resultado = monto * tasaDeCambio;
+    document.getElementById('resultado').textContent = resultado.toFixed(2) + ' dírhams marroquíes';
+  }
+
+  // Escucha el evento de cambio en el input para actualizar el resultado
+  document.getElementById('monto').addEventListener('input', convertirADirhams);
+
+  // Llama a la función inicialmente
+  convertirADirhams();
+
